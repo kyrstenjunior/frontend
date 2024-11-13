b3ca594e938ed348fbc13dda164da0bb59a6c8b2
kyrstenjunior <kyrstenjr@gmail.com>
2024-11-12 21:20:08 -0300

    transformando a lib cookies em uma const await para corrigir o não salvamento dos cookies do server para o client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/app/dashboard/components/ui/Input";
 import { Button } from "@/app/dashboard/components/ui/Button";
 
 import { api } from "../services/api";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function Home() {
@@ -34,13 +35,14 @@ export default function Home() {
       }
 
       const expressTime = 60 * 60 * 24 * 30 * 1000; // significa 30 dias
-      document.cookie = `
-        session=${response.data.token};
-        path=/;
-        max-age=${expressTime};
-        secure;
-        samesite=strict;
-      `;
+      const cookieStore = await cookies();
+
+      cookieStore.set("session", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: true // quando estiver em produção, é só utilizar true nesta propriedade
+      })
     } catch (error) {
       console.error(error);
       return;
